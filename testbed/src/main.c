649f836e1a7de376446bfddaea360f15658ad3aa
Michaël Desmedt <drayan293@gmail.com>
2023-08-18 00:31:18 +0200

    Basic logging system and tests

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,8 +1,17 @@
-#include <test.h>
+#include <core/logger.h>
 
 int main(void)
 {
-    print_int(42);
+    initialize_logging();
+
+    AFATAL("A test message: %f", 3.14f);
+    AERROR("A test message: %f", 3.14f);
+    AWARN("A test message: %f", 3.14f);
+    AINFO("A test message: %f", 3.14f);
+    ADEBUG("A test message: %f", 3.14f);
+    ATRACE("A test message: %f", 3.14f);
+
+    shutdown_logging();
 
     return 0;
 }
