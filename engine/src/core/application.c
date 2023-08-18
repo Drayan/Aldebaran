23b5592facde5c0407b8f364815e880bf3bf16a4
Michaël Desmedt <drayan293@gmail.com>
2023-08-18 18:38:15 +0200

    Basic application layer.

---
+++ engine/src/core/application.c
@@ -0,0 +1,67 @@
+#include "application.h"
+
+#include "logger.h"
+#include "platform/platform.h"
+
+typedef struct application_state
+{
+    b8 is_running;
+    b8 is_suspended;
+    platform_state platform;
+    i16 width;
+    i16 height;
+    f64 last_time;
+} application_state;
+
+static b8 initialized = FALSE;
+static application_state app_state;
+
+b8 application_create(application_config *config)
+{
+    if(initialized)
+    {
+        AERROR("application_create called more than once.");
+        return FALSE;
+    }
+
+    // Initialize subsystems.
+    initialize_logging();
+
+    // TODO: Remove this.
+    AFATAL("A test message: %f", 3.14f);
+    AERROR("A test message: %f", 3.14f);
+    AWARN("A test message: %f", 3.14f);
+    AINFO("A test message: %f", 3.14f);
+    ADEBUG("A test message: %f", 3.14f);
+    ATRACE("A test message: %f", 3.14f);
+
+    app_state.is_running = TRUE;
+    app_state.is_suspended = FALSE;
+
+    if(!platform_startup(&app_state.platform, config->name, config->start_pos_x, config->start_pos_y, config->start_width, config->start_height))
+    {
+        return FALSE;
+    }
+
+    initialized = TRUE;
+
+    return TRUE;
+}
+
+b8 application_run()
+{
+    while(app_state.is_running)
+    {
+        if(!platform_pump_message(&app_state.platform))
+        {
+            app_state.is_running = FALSE;
+        }
+    }
+
+    app_state.is_running = FALSE;
+
+    platform_shutdown(&app_state.platform);
+    shutdown_logging();
+
+    return TRUE;
+}
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,25 +1,18 @@
 #include <core/logger.h>
-#include <platform/platform.h>
+#include <core/application.h>
 
 int main(void)
 {
-    AFATAL("A test message: %f", 3.14f);
-    AERROR("A test message: %f", 3.14f);
-    AWARN("A test message: %f", 3.14f);
-    AINFO("A test message: %f", 3.14f);
-    ADEBUG("A test message: %f", 3.14f);
-    ATRACE("A test message: %f", 3.14f);
+    application_config config;
+    config.start_pos_x = 100;
+    config.start_pos_y = 100;
+    config.start_width = 1280;
+    config.start_height = 720;
+    config.name = "Aldebaran Engine Testbed";
 
-    platform_state state;
-    if(platform_startup(&state, "Aldebaran engine testbed", 100, 100, 1200, 720))
-    {
-        while(TRUE)
-        {
-            platform_pump_message(&state);
-        }
-    }
+    application_create(&config);
 
-    platform_shutdown(&state);
+    application_run();
 
     return 0;
 }
