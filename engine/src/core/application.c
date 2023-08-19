a31bdea4ede8192c0dec50b3ea842354cc7607a5
Michaël Desmedt <drayan293@gmail.com>
2023-08-19 21:33:03 +0200

    Input subsystem scaffolding.

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -5,6 +5,7 @@
 #include "platform/platform.h"
 #include "core/amemory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state
 {
@@ -20,11 +21,11 @@ typedef struct application_state
 static b8 initialized = FALSE;
 static application_state app_state;
 
-b8 application_create(game *game_inst)
+b8 application_create( game *game_inst )
 {
-    if (initialized)
+    if( initialized )
     {
-        AERROR("application_create called more than once.");
+        AERROR( "application_create called more than once." );
         return FALSE;
     }
 
@@ -33,41 +34,43 @@ b8 application_create(game *game_inst)
     // Initialize subsystems.
     initialize_logging();
 
-    if (!initialize_events())
+    if( !initialize_events() )
     {
-        AFATAL("Event system failed initialization. Application cannot continue.");
+        AFATAL( "Event system failed initialization. Application cannot continue." );
         return FALSE;
     }
 
+    initialize_inputs();
+
     // TODO: Remove this.
-    AFATAL("A test message: %f", 3.14f);
-    AERROR("A test message: %f", 3.14f);
-    AWARN("A test message: %f", 3.14f);
-    AINFO("A test message: %f", 3.14f);
-    ADEBUG("A test message: %f", 3.14f);
-    ATRACE("A test message: %f", 3.14f);
+    AFATAL( "A test message: %f", 3.14f );
+    AERROR( "A test message: %f", 3.14f );
+    AWARN( "A test message: %f", 3.14f );
+    AINFO( "A test message: %f", 3.14f );
+    ADEBUG( "A test message: %f", 3.14f );
+    ATRACE( "A test message: %f", 3.14f );
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
 
-    if (!platform_startup(&app_state.platform,
-                          game_inst->app_config.name,
-                          game_inst->app_config.start_pos_x,
-                          game_inst->app_config.start_pos_y,
-                          game_inst->app_config.start_width,
-                          game_inst->app_config.start_height))
+    if( !platform_startup( &app_state.platform,
+        game_inst->app_config.name,
+        game_inst->app_config.start_pos_x,
+        game_inst->app_config.start_pos_y,
+        game_inst->app_config.start_width,
+        game_inst->app_config.start_height ) )
     {
         return FALSE;
     }
 
     // Initialize the game.
-    if (!app_state.game_inst->initialize(app_state.game_inst))
+    if( !app_state.game_inst->initialize( app_state.game_inst ) )
     {
-        AFATAL("Game failed to initialize.");
+        AFATAL( "Game failed to initialize." );
         return FALSE;
     }
 
-    app_state.game_inst->on_resize(app_state.game_inst, app_state.width, app_state.height);
+    app_state.game_inst->on_resize( app_state.game_inst, app_state.width, app_state.height );
 
     initialized = TRUE;
 
@@ -76,39 +79,46 @@ b8 application_create(game *game_inst)
 
 b8 application_run()
 {
-    AINFO(get_memory_usage_str());
+    AINFO( get_memory_usage_str() );
 
-    while (app_state.is_running)
+    while( app_state.is_running )
     {
-        if (!platform_pump_message(&app_state.platform))
+        if( !platform_pump_message( &app_state.platform ) )
         {
             app_state.is_running = FALSE;
         }
 
-        if (!app_state.is_suspended)
+        if( !app_state.is_suspended )
         {
-            if (!app_state.game_inst->update(app_state.game_inst, (f32)0))
+            if( !app_state.game_inst->update( app_state.game_inst, ( f32 )0 ) )
             {
-                AFATAL("Game update failed, shutting down.");
+                AFATAL( "Game update failed, shutting down." );
                 app_state.is_running = FALSE;
                 break;
             }
 
             // Call the game's render routine.
-            if (!app_state.game_inst->render(app_state.game_inst, (f32)0))
+            if( !app_state.game_inst->render( app_state.game_inst, ( f32 )0 ) )
             {
-                AFATAL("Game render failed, shutting down.");
+                AFATAL( "Game render failed, shutting down." );
                 app_state.is_running = FALSE;
                 break;
             }
+
+            // NOTE: Input update/state copying should always be handled after any
+            // input should be recorded; I.E. before this line.
+            // As a safety, input is the last thing to be updated before this
+            // frame ends.
+            update_inputs( ( f64 )0 );
         }
     }
 
     app_state.is_running = FALSE;
 
+    shutdown_inputs();
     shutdown_events();
 
-    platform_shutdown(&app_state.platform);
+    platform_shutdown( &app_state.platform );
     shutdown_logging();
 
     return TRUE;
---
+++ engine/src/core/input.c
@@ -0,0 +1,222 @@
+#include "core/input.h"
+#include "core/event.h"
+#include "core/amemory.h"
+#include "core/logger.h"
+#include "input.h"
+
+typedef struct keyboard_state
+{
+    b8 keys[256];
+} keyboard_state;
+
+typedef struct mouse_state
+{
+    i16 x;
+    i16 y;
+    b8 buttons[MOUSE_BUTTON_MAX_BUTTONS];
+} mouse_state;
+
+typedef struct input_state
+{
+    keyboard_state keyboard_current;
+    keyboard_state keyboard_previous;
+    mouse_state mouse_current;
+    mouse_state mouse_previous;
+} input_state;
+
+// Internal input state
+static b8 is_initialized = FALSE;
+static input_state state = {};
+
+void initialize_inputs()
+{
+    azero_memory( &state, sizeof( input_state ) );
+    is_initialized = TRUE;
+
+    AINFO( "Input subsystem initialized." );
+}
+
+void shutdown_inputs()
+{
+    // TODO: Add shutdown routines when needed.
+    is_initialized = FALSE;
+}
+
+void update_inputs( f64 delta_time )
+{
+    if( !is_initialized )
+    {
+        return;
+    }
+
+    // Copy current states to previous states.
+    acopy_memory( &state.keyboard_previous, &state.keyboard_current, sizeof( keyboard_state ) );
+    acopy_memory( &state.mouse_previous, &state.mouse_current, sizeof( mouse_state ) );
+}
+
+void input_process_key( keys key, b8 pressed )
+{
+    // Only handle this if the state actually changed.
+    if( state.keyboard_current.keys[key] != pressed )
+    {
+        // Update internal state.
+        state.keyboard_current.keys[key] = pressed;
+
+        // Fire off an event for immediate processing.
+        event_context context;
+        context.data.u16[0] = key;
+        event_fire( pressed ? EVENT_CODE_KEY_PRESSED : EVENT_CODE_KEY_RELEASED, 0, context );
+    }
+}
+
+void input_process_mouse_button( mouse_buttons button, b8 pressed )
+{
+    // If the state changed, fire an event.
+    if( state.mouse_current.buttons[button] != pressed )
+    {
+        state.mouse_current.buttons[button] = pressed;
+
+        // Fire the event.
+        event_context context;
+        context.data.u16[0] = button;
+        event_fire( pressed ? EVENT_CODE_MOUSE_BUTTON_PRESSED : EVENT_CODE_MOUSE_BUTTON_RELEASED,
+            0,
+            context );
+    }
+}
+
+void input_process_mouse_move( i16 x, i16 y )
+{
+    // Only process if actually different.
+    if( state.mouse_current.x != x || state.mouse_current.y != y )
+    {
+        // NOTE: Enable this for debugging.
+        ADEBUG( "Mouse pos: %i, %i!", x, y );
+
+        // Update the internal state.
+        state.mouse_current.x = x;
+        state.mouse_current.y = y;
+
+        // Fire the event.
+        event_context context;
+        context.data.u16[0] = x;
+        context.data.u16[1] = y;
+        event_fire( EVENT_CODE_MOUSE_MOVED, 0, context );
+    }
+}
+
+void input_process_mouse_wheel( i8 z_delta )
+{
+    // NOTE: No internal state to update.
+
+    // Fire the event.
+    event_context context;
+    context.data.u8[0] = z_delta;
+    event_fire( EVENT_CODE_MOUSE_WHEEL, 0, context );
+}
+
+b8 input_is_key_down( keys key )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.keyboard_current.keys[key] == TRUE;
+}
+
+b8 input_is_key_up( keys key )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.keyboard_current.keys[key] == FALSE;
+}
+
+b8 input_was_key_down( keys key )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.keyboard_previous.keys[key] == TRUE;
+}
+
+b8 input_was_key_up( keys key )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.keyboard_previous.keys[key] == FALSE;
+}
+
+b8 input_is_mouse_button_down( mouse_buttons button )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.mouse_current.buttons[button] == TRUE;
+}
+
+b8 input_is_mouse_button_up( mouse_buttons button )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.mouse_current.buttons[button] == FALSE;
+}
+
+b8 input_was_mouse_button_down( mouse_buttons button )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.mouse_previous.buttons[button] == TRUE;
+}
+
+b8 input_was_mouse_button_up( mouse_buttons button )
+{
+    if( !is_initialized )
+    {
+        return FALSE;
+    }
+
+    return state.mouse_previous.buttons[button] == FALSE;
+}
+
+void input_get_mouse_position( i32 *x, i32 *y )
+{
+    if( !is_initialized )
+    {
+        *x = 0;
+        *y = 0;
+        return;
+    }
+
+    *x = state.mouse_current.x;
+    *y = state.mouse_current.y;
+}
+
+void input_get_previous_mouse_position( i32 *x, i32 *y )
+{
+    if( !is_initialized )
+    {
+        *x = 0;
+        *y = 0;
+        return;
+    }
+
+    *x = state.mouse_previous.x;
+    *y = state.mouse_previous.y;
+}
