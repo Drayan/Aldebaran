1bd90db4fdcba868d7ba748885232020f17d6255
Michaël Desmedt <drayan293@gmail.com>
2023-08-21 01:56:45 +0200

    Clock implementation

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -6,6 +6,7 @@
 #include "core/amemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
 
 typedef struct application_state
 {
@@ -15,20 +16,21 @@ typedef struct application_state
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
 static b8 initialized = FALSE;
 static application_state app_state;
 
-b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context);
-b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context);
+b8 application_on_event(u16 code, void *sender, void *listener_inst, event_context context);
+b8 application_on_key(u16 code, void *sender, void *listener_inst, event_context context);
 
-b8 application_create( game *game_inst )
+b8 application_create(game *game_inst)
 {
-    if( initialized )
+    if (initialized)
     {
-        AERROR( "application_create called more than once." );
+        AERROR("application_create called more than once.");
         return FALSE;
     }
 
@@ -37,9 +39,9 @@ b8 application_create( game *game_inst )
     // Initialize subsystems.
     initialize_logging();
 
-    if( !initialize_events() )
+    if (!initialize_events())
     {
-        AFATAL( "Event system failed initialization. Application cannot continue." );
+        AFATAL("Event system failed initialization. Application cannot continue.");
         return FALSE;
     }
 
@@ -51,34 +53,34 @@ b8 application_create( game *game_inst )
     initialize_inputs();
 
     // TODO: Remove this.
-    AFATAL( "A test message: %f", 3.14f );
-    AERROR( "A test message: %f", 3.14f );
-    AWARN( "A test message: %f", 3.14f );
-    AINFO( "A test message: %f", 3.14f );
-    ADEBUG( "A test message: %f", 3.14f );
-    ATRACE( "A test message: %f", 3.14f );
+    AFATAL("A test message: %f", 3.14f);
+    AERROR("A test message: %f", 3.14f);
+    AWARN("A test message: %f", 3.14f);
+    AINFO("A test message: %f", 3.14f);
+    ADEBUG("A test message: %f", 3.14f);
+    ATRACE("A test message: %f", 3.14f);
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
 
-    if( !platform_startup( &app_state.platform,
-        game_inst->app_config.name,
-        game_inst->app_config.start_pos_x,
-        game_inst->app_config.start_pos_y,
-        game_inst->app_config.start_width,
-        game_inst->app_config.start_height ) )
+    if (!platform_startup(&app_state.platform,
+                          game_inst->app_config.name,
+                          game_inst->app_config.start_pos_x,
+                          game_inst->app_config.start_pos_y,
+                          game_inst->app_config.start_width,
+                          game_inst->app_config.start_height))
     {
         return FALSE;
     }
 
     // Initialize the game.
-    if( !app_state.game_inst->initialize( app_state.game_inst ) )
+    if (!app_state.game_inst->initialize(app_state.game_inst))
     {
-        AFATAL( "Game failed to initialize." );
+        AFATAL("Game failed to initialize.");
         return FALSE;
     }
 
-    app_state.game_inst->on_resize( app_state.game_inst, app_state.width, app_state.height );
+    app_state.game_inst->on_resize(app_state.game_inst, app_state.width, app_state.height);
 
     initialized = TRUE;
 
@@ -87,37 +89,74 @@ b8 application_create( game *game_inst )
 
 b8 application_run()
 {
-    AINFO( get_memory_usage_str() );
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
 
-    while( app_state.is_running )
+    f64 running_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
+    AINFO(get_memory_usage_str());
+
+    while (app_state.is_running)
     {
-        if( !platform_pump_message( &app_state.platform ) )
+        if (!platform_pump_message(&app_state.platform))
         {
             app_state.is_running = FALSE;
         }
 
-        if( !app_state.is_suspended )
+        if (!app_state.is_suspended)
         {
-            if( !app_state.game_inst->update( app_state.game_inst, ( f32 )0 ) )
+            // Update the clock and get delta time.
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.last_time);
+            f64 frame_start_time = platform_get_absolute_time();
+
+            if (!app_state.game_inst->update(app_state.game_inst, (f32)delta))
             {
-                AFATAL( "Game update failed, shutting down." );
+                AFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
             // Call the game's render routine.
-            if( !app_state.game_inst->render( app_state.game_inst, ( f32 )0 ) )
+            if (!app_state.game_inst->render(app_state.game_inst, (f32)delta))
             {
-                AFATAL( "Game render failed, shutting down." );
+                AFATAL("Game render failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
+            }
+
+            // Figure out how long the frame took and, if below
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            if (remaining_seconds > 0)
+            {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                // If there is time left, give it back to the os.
+                b8 limit_frames = FALSE;
+                if (remaining_ms > 0 && limit_frames)
+                {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                frame_count++;
             }
 
             // NOTE: Input update/state copying should always be handled after any
             // input should be recorded; I.E. before this line.
             // As a safety, input is the last thing to be updated before this
             // frame ends.
-            update_inputs( ( f64 )0 );
+            update_inputs(delta);
+
+            // Update last time
+            clock_update(&app_state.clock);
         }
     }
 
@@ -132,13 +171,13 @@ b8 application_run()
 
     shutdown_events();
 
-    platform_shutdown( &app_state.platform );
+    platform_shutdown(&app_state.platform);
     shutdown_logging();
 
     return TRUE;
 }
 
-b8 application_on_event(u16 code, void* sender, void* listener_inst, event_context context)
+b8 application_on_event(u16 code, void *sender, void *listener_inst, event_context context)
 {
     switch (code)
     {
@@ -146,7 +185,7 @@ b8 application_on_event(u16 code, void* sender, void* listener_inst, event_conte
         AINFO("EVENT_CODE_APPLICATION_QUIT received, shutting down.");
         app_state.is_running = FALSE;
         return TRUE;
-    
+
     default:
         break;
     }
@@ -154,12 +193,12 @@ b8 application_on_event(u16 code, void* sender, void* listener_inst, event_conte
     return FALSE;
 }
 
-b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context context)
+b8 application_on_key(u16 code, void *sender, void *listener_inst, event_context context)
 {
-    if(code == EVENT_CODE_KEY_PRESSED)
+    if (code == EVENT_CODE_KEY_PRESSED)
     {
         u16 key_code = context.data.u16[0];
-        if(key_code == KEY_ESCAPE)
+        if (key_code == KEY_ESCAPE)
         {
             // NOTE: Technically firing an event to itself, but there may be other listeners.
             event_context context = {};
@@ -168,7 +207,7 @@ b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context
             // Block anything else from processing this.
             return TRUE;
         }
-        else if(key_code == KEY_A) 
+        else if (key_code == KEY_A)
         {
             // Example on checking for a key
             ADEBUG("Explicit - A key pressed!");
@@ -178,10 +217,10 @@ b8 application_on_key(u16 code, void* sender, void* listener_inst, event_context
             ADEBUG("'%c' key pressed in window.", key_code);
         }
     }
-    else if(code == EVENT_CODE_KEY_RELEASED)
+    else if (code == EVENT_CODE_KEY_RELEASED)
     {
         u16 key_code = context.data.u16[0];
-        if(key_code == KEY_B)
+        if (key_code == KEY_B)
         {
             // Example on checking for a key.
             ADEBUG("Explicit - B key released!");
