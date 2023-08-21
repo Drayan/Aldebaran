ec6d14e505cbaac7e8b979c05c5869eb33b4321e
Michaël Desmedt <drayan293@gmail.com>
2023-08-21 19:28:26 +0200

    Fixing VULKAN_INCOMPATIBLE_DRIVER on OSX by enabling required extensions.

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -12,18 +12,26 @@ b8 vulkan_renderer_backend_initialize(renderer_backend *backend, const char *app
     context.allocator = 0;
 
     VkApplicationInfo app_info = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
-    app_info.apiVersion = VK_API_VERSION_1_2;
+    app_info.apiVersion = VK_API_VERSION_1_3;
     app_info.pApplicationName = application_name;
     app_info.applicationVersion = VK_MAKE_API_VERSION(1, 0, 0, 0);
     app_info.pEngineName = "Aldebaran Engine";
     app_info.engineVersion = VK_MAKE_API_VERSION(1, 0, 0, 0);
 
+    // NOTE: Starting from Vulkan 1.3.216, theses extensions MUST be enabled on OSX to being able
+    // to initialize the instance. As I'll probably refactor these later, and I'll probably
+    // have to make a system to enabled extensions on other platforms, I'm not going to differentiace
+    // platform now.
+    const char *extension_names[] = {VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+                                     VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME};
+
     VkInstanceCreateInfo create_info = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
     create_info.pApplicationInfo = &app_info;
-    create_info.enabledExtensionCount = 0;
-    create_info.ppEnabledExtensionNames = 0;
+    create_info.enabledExtensionCount = 2;
+    create_info.ppEnabledExtensionNames = extension_names;
     create_info.enabledLayerCount = 0;
     create_info.ppEnabledLayerNames = 0;
+    create_info.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
     VkResult result = vkCreateInstance(&create_info, context.allocator, &context.instance);
     if (result != VK_SUCCESS)
